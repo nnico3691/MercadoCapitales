64208dd642b8e3f71e5429dda0d6e74cd7ba3b29
Nicolas Messa <nnico3691@outlook.es>
2022-12-27 13:07:11 -0300

    Agregue Modelo matcheado con la BD y configuración de API.Precio
    Agregue Modelo matcheado con la BD y configuración de API.Precio

---
+++ MercadoCapitales/MercadoCapitales.API.Precios/Migrations/20221227160232_MigracionInicial.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MercadoCapitales.API.Precios.Migrations
+{
+    public partial class MigracionInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CotizacionAccion",
+                columns: table => new
+                {
+                    PrecioId = table.Column<Guid>(nullable: false),
+                    Ticker = table.Column<string>(nullable: true),
+                    Precio = table.Column<float>(nullable: true),
+                    VarPorcentual = table.Column<float>(nullable: true),
+                    PrecioA = table.Column<float>(nullable: true),
+                    CCompra = table.Column<float>(nullable: true),
+                    PCompra = table.Column<float>(nullable: true),
+                    CVenta = table.Column<float>(nullable: true),
+                    Min = table.Column<float>(nullable: true),
+                    Max = table.Column<float>(nullable: true),
+                    VolNom = table.Column<string>(nullable: true),
+                    Monto = table.Column<float>(nullable: true),
+                    Oper = table.Column<int>(nullable: true),
+                    Hora = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CotizacionAccion", x => x.PrecioId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CotizacionAccion");
+        }
+    }
+}
