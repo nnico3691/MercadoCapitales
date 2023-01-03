3fe5538dee1c258b49849b56a746a06e7331bc1d
Nicolas Messa <nnico3691@outlook.es>
2023-01-03 16:49:12 -0300

    GITIGNORE MÁS LOGICA DE PRECIO EN EL FRONT
    GITIGNORE MÁS LOGICA DE PRECIO EN EL FRONT

--- GestorMercadoCapitales/GestorMercadoCapitales/Controllers/HomeController.cs
+++ GestorMercadoCapitales/GestorMercadoCapitales/Controllers/HomeController.cs
@@ -9,6 +9,9 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using Newtonsoft.Json;
 using Microsoft.AspNetCore.Http;
+using Primary.Data;
+using Primary;
+using System.Threading;
 
 namespace GestorMercadoCapitales.Controllers
 {
@@ -53,6 +56,13 @@ namespace GestorMercadoCapitales.Controllers
 
         private List<CotizacionAccion> GetCotizaciones()
         {
+
+            try
+            {
+                ThreadPool.QueueUserWorkItem(RunSocket, new object[] { });
+            }
+            catch { }
+
             string url = "http://localhost:22684/api/Precio/GetPreciosAccion";
             var parames = new Dictionary<string, string>();
             HttpClientHandler clientHandler = new HttpClientHandler();
@@ -73,7 +83,90 @@ namespace GestorMercadoCapitales.Controllers
             return data;
 
         }
-        
+
+        private static async void RunSocket(object state)
+        {
+            Console.WriteLine("Connecting to ReMarkets...");
+
+            var api = new Api(Api.DemoEndpoint);
+            await api.Login(Api.DemoUsername, Api.DemoPassword);
+
+            // Get a all dollar futures
+            Console.WriteLine("Getting available instruments...");
+
+            var allIInstruments = await api.GetAllInstruments();
+
+            var symbols = new[]
+            {
+                "DLR/ENE23",
+                "ORO/ENE23",
+                "ORO/MAR23",
+                "ORO/MAY23",
+                "YPFD/FEB23",
+                "RFX20/FEB23",
+                "GGAL/FEB23",
+                "WTI/ENE23",
+                "WTI/MAR23",
+                "WTI/MAY23"
+
+
+            };
+            var dollarFuture = allIInstruments.Where(c => symbols.Contains(c.Symbol));
+
+            // Subscribe to bids and offers
+            var entries = new[] { Entry.Offers, Entry.Close, Entry.EffectiveVolume, Entry.NominalVolume, Entry.Bids };
+
+            Console.WriteLine("Connecting to market data...");
+
+            var socket = api.CreateMarketDataSocket(dollarFuture, entries, 1, 2);
+            socket.OnData = OnMarketData;
+
+            Console.WriteLine("Start Socket...");
+
+            var socketTask = await socket.Start();
+
+            socketTask.Wait();
+            await socketTask;
+
+
+        }
+        private static void OnMarketData(Api api, MarketData marketData)
+        {
+            try
+            {
+                var bid = default(decimal);
+                var offer = default(decimal);
+
+                var bidSize = default(decimal);
+                var offerSize = default(decimal);
+
+                var nominalVolume = default(decimal?);
+
+                foreach (var trade in marketData.Data.Bids)
+                {
+                    bid = trade.Price;
+                    bidSize = trade.Size;
+                }
+
+                foreach (var trade in marketData.Data.Offers)
+                {
+                    offer = trade.Price;
+                    offerSize = trade.Size;
+                }
+
+                nominalVolume = marketData.Data.NominalVolume;
+
+                if (bid > 0 || offer > 0)
+                    Console.WriteLine($"({marketData.Timestamp}) " +
+                                      $"{marketData.InstrumentId.Symbol} -> " +
+                                      $"Vol.C: {bidSize} ; Compra: {bid} ; Venta: {offer} ; Vol.V: {offerSize}; Vol. Operado: nominalVolume;"
+                    );
+
+            }
+            catch { }
+
+        }
+
 
     }
 }
