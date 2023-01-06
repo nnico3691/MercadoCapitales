330f5f1116966c59ea644c857dd7a93f0211a017
jorger <jorgerlaneri@gmail.com>
2023-01-06 10:36:02 -0300

    Commit
    -

--- GestorMercadoCapitales/GestorMercadoCapitales/Controllers/MtbaRfxController.cs
+++ GestorMercadoCapitales/GestorMercadoCapitales/Controllers/MtbaRfxController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,13 +11,14 @@ namespace GestorMercadoCapitales.Controllers
 {
     public class MtbaRfxController : Controller
     {
+        private IConfiguration _configuration;
         public ActionResult Futuros_Financieros()
         {
             if (HttpContext.Session.GetString("Socket") != "Iniciado")
             {
                 try
                 {
-                    Socket socket = new Socket();
+                    Socket socket = new Socket(_configuration);
 
                     ThreadPool.QueueUserWorkItem(socket.RunSocket, new object[] { });
                     HttpContext.Session.SetString("Socket", "Iniciado");
