2684a2e73c442f6892c1ddccaa35580c9678bbcf
jorger <jorgerlaneri@gmail.com>
2023-01-06 09:56:50 -0300

    Primera version de Encuesta del Inversor
    -

---
+++ GestorMercadoCapitales/GestorMercadoCapitales/Controllers/Encuesta_InversorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestorMercadoCapitales.Controllers
+{
+    public class Encuesta_InversorController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    
+    }
+}
---
+++ GestorMercadoCapitales/GestorMercadoCapitales/Models/EncuestaInversor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace GestorMercadoCapitales.Models
+{
+    public class EncuestaInversor
+    {
+        public int id_encuest_pregunt { get; set; }
+        public string titulo { get; set; }
+        public string preguntas { get; set; }
+
+
+    }
+
+    public class EncuestaInversorPreg
+    {
+        public static List<EncuestaInversor> ListaEncuesta;
+    }
+
+
+}
