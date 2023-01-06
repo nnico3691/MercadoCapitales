a5cc02e2e6b7b90810e978a6a536aa4961aec10c
jorger <jorgerlaneri@gmail.com>
2023-01-06 10:59:30 -0300

    Revert "Commit"
    This reverts commit 330f5f1116966c59ea644c857dd7a93f0211a017.

--- GestorMercadoCapitales/GestorMercadoCapitales/Controllers/MtbaRfxController.cs
+++ GestorMercadoCapitales/GestorMercadoCapitales/Controllers/MtbaRfxController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,14 +10,13 @@ namespace GestorMercadoCapitales.Controllers
 {
     public class MtbaRfxController : Controller
     {
-        private IConfiguration _configuration;
         public ActionResult Futuros_Financieros()
         {
             if (HttpContext.Session.GetString("Socket") != "Iniciado")
             {
                 try
                 {
-                    Socket socket = new Socket(_configuration);
+                    Socket socket = new Socket();
 
                     ThreadPool.QueueUserWorkItem(socket.RunSocket, new object[] { });
                     HttpContext.Session.SetString("Socket", "Iniciado");
