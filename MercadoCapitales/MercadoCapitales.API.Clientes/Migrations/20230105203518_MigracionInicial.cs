143d7e5f534f9eeea78954f80a42cb9526430d54
Nicolas Messa <nnico3691@outlook.es>
2023-01-06 10:38:29 -0300

    Prueba girignore
    Prueba girignore

---
+++ MercadoCapitales/MercadoCapitales.API.Clientes/Migrations/20230105203518_MigracionInicial.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MercadoCapitales.API.Clientes.Migrations
+{
+    public partial class MigracionInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    ClienteId = table.Column<Guid>(nullable: false),
+                    Nombre = table.Column<string>(nullable: true),
+                    Apellido = table.Column<string>(nullable: true),
+                    TipoDNI = table.Column<string>(nullable: true),
+                    DNI = table.Column<string>(nullable: true),
+                    Telefono = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.ClienteId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ClienteEncuestas",
+                columns: table => new
+                {
+                    ClienteEncuestasId = table.Column<Guid>(nullable: false),
+                    EncuestaRespuesta = table.Column<Guid>(nullable: true),
+                    ClieKey = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClienteEncuestas", x => x.ClienteEncuestasId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EncuestaPregunta",
+                columns: table => new
+                {
+                    EncuestaPreguntaId = table.Column<Guid>(nullable: false),
+                    Titulo = table.Column<string>(nullable: true),
+                    Pregunta = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EncuestaPregunta", x => x.EncuestaPreguntaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EncuestaRespuesta",
+                columns: table => new
+                {
+                    EncuestaRespuestaId = table.Column<Guid>(nullable: false),
+                    Respuesta = table.Column<string>(nullable: true),
+                    Puntos = table.Column<int>(nullable: false),
+                    NameInput = table.Column<string>(nullable: true),
+                    TipoInput = table.Column<string>(nullable: true),
+                    EncuestaPregunta = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EncuestaRespuesta", x => x.EncuestaRespuestaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Login",
+                columns: table => new
+                {
+                    LoginId = table.Column<Guid>(nullable: false),
+                    Usuario = table.Column<string>(nullable: true),
+                    Clave = table.Column<string>(nullable: true),
+                    Cliente = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Login", x => x.LoginId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cliente");
+
+            migrationBuilder.DropTable(
+                name: "ClienteEncuestas");
+
+            migrationBuilder.DropTable(
+                name: "EncuestaPregunta");
+
+            migrationBuilder.DropTable(
+                name: "EncuestaRespuesta");
+
+            migrationBuilder.DropTable(
+                name: "Login");
+        }
+    }
+}
---
+++ MercadoCapitales/MercadoCapitales.API.Clientes/Modelo/EncuestaPregunta.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace MercadoCapitales.API.Clientes.Modelo
+{
+    public class EncuestaPregunta
+    {
+        [Key]
+        public Guid? EncuestaPreguntaId { get; set; }
+        public string Titulo { get; set; }
+        public string Pregunta { get; set; }
+    }
+}
